90253ebdabea1ef7943a14150345d6cbd7d03999
Aleksandr_Denisov <aleksandr_denisov@epam.com>
2022-10-14 14:08:08 +0200

    fix of the build with pods issue

--- lib/ui/widgets/movie_card/tv_movie_card.dart
+++ lib/ui/widgets/movie_card/tv_movie_card.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_tv/domain/movie.dart';
 import 'package:flutter_tv/ui/focus/extensions.dart';
 import 'package:flutter_tv/ui/widgets/movie_card/mobile_movie_card.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 class TvMovieCard extends StatefulWidget {
   final int index;
@@ -33,6 +35,7 @@ class _TvMovieCardState extends State<TvMovieCard> {
       onKey: (_, event) {
         if (widget.onTap != null && event.hasSubmitIntent) {
           widget.onTap!();
+          _incrementCounter();
           return KeyEventResult.handled;
         }
         return KeyEventResult.ignored;
@@ -55,5 +58,10 @@ class _TvMovieCardState extends State<TvMovieCard> {
         ),
       ),
     );
+  }
+
+  Future<void> _incrementCounter() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('counter', counter);
   }
 }
