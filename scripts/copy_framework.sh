54bf2fcb7a99f410ca89938a542899fa714cac30
denisovav <denisov.shureg@gmail.com>
2024-05-04 20:18:38 +0200

    Update for arm64 macs

--- scripts/copy_framework.sh
+++ scripts/copy_framework.sh
@@ -15,6 +15,9 @@ if [ "$1" == "release" ]; then
 elif [[ "$1" == "debug_sim" ]] ; then
 	echo "Coping Flutter.framework (debug-simulator)..."
 	DEVICE_TOOLS=$FLUTTER_LOCAL_ENGINE/out/ios_debug_sim_unopt/clang_x64
+elif [[ "$1" == "debug_sim_arm64" ]] ; then
+	echo "Coping Flutter.framework (debug-simulator-arm64)..."
+	DEVICE_TOOLS=$FLUTTER_LOCAL_ENGINE/out/ios_debug_sim_unopt_arm64/clang_x64
 else
 	#debug
 	echo "Coping Flutter.framework (debug)..."
--- scripts/run_apple_tv.sh
+++ scripts/run_apple_tv.sh
@@ -1,9 +1,25 @@
+#!/bin/bash
+# Usage: ./run_apple_tv.sh [ENGINE_TYPE]
+#  ENGINE_TYPE: 
+#  'debug_sim' - engine for x86_64 Mac apple tv simulator
+#  'debug_sim_arm64' - engine for arm64 Mac apple tv simulator
+#  'debug' - engine for real apple tv device, debug mode
+#  'release' - engine for real apple tv device, release mode
+
+# engine_type is optional, defaults to 'debug_sim_arm64'
+
 set -x
 
 #added all scripts here to run appletv locally from one script
 
+ENGINE_TYPE=${1:-debug_sim_arm64}
+
 # set your own path to custom engine here
 export FLUTTER_LOCAL_ENGINE=/Users/Aleksandr_Denisov/Work/engine/src
+
+if [ -d "_ios" ]; then
+sh scripts/switch_target.sh ios
+fi
 
 # Clean old mess
 fvm flutter clean
@@ -17,18 +33,13 @@ sh scripts/switch_target.sh tvos
 
 cd ios
 
-#choose the necessary type of the engine
-#  'debug_sim' - engine for apple tv simulator
-#  'debug' - engine for real apple tv device, debug mode
-#  'release' - engine for real apple tv device, release mode
-
 fvm flutter pub get
 
 pod install
 
-sh ../scripts/copy_framework.sh debug_sim FLUTTER_LOCAL_ENGINE
+sh ../scripts/copy_framework.sh $ENGINE_TYPE FLUTTER_LOCAL_ENGINE
 
 #add local engine in xcode project
-sed -i "s#FLUTTER_LOCAL_ENGINE[[:space:]]=[[:space:]].*;#FLUTTER_LOCAL_ENGINE = \"${FLUTTER_LOCAL_ENGINE}\";#g" Runner.xcodeproj/project.pbxproj
+sed -i "" "s#FLUTTER_LOCAL_ENGINE[[:space:]]=[[:space:]].*;#FLUTTER_LOCAL_ENGINE = \"${FLUTTER_LOCAL_ENGINE}\";#g" Runner.xcodeproj/project.pbxproj
 
 open Runner.xcworkspace
